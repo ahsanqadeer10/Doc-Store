9c3c23041882d10cc21330e219627b0d140173a8
Ahsan Qadeer <ahsan.qadeer10@hotmail.com>
2021-11-13 01:32:15 +0500

    PostgreSQL added.

--- docstore/models.py
+++ docstore/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Folder(models.Model):
+      name = models.CharField(max_length=100, unique=True, null=False)
+      topic = models.ManyToManyField("Topic", related_name="folders")
+
+class Document(models.Model):
+      name = models.CharField(max_length=100, unique=True, null=False)
+      folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name="documents")
+      topic = models.ManyToManyField("Topic", related_name="documents")
+
+class Topic(models.Model):
+      name = models.CharField(max_length=100, unique=True, null=False)
